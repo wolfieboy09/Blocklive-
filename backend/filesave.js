dfaf26dfef668e8879e74b68c624b149c0b871f1
micahpowch <meechapooch@gmail.com>
2024-03-27 21:46:53 -0700

    bruh

--- backend/filesave.js
+++ backend/filesave.js
@@ -47,7 +47,7 @@ export async function saveMapToFolderAsync(obj, dir) {
           entry[0] = sanitize(entry[0] + '')
           if(entry[0] == '') {return}
           let file = await fsp.open(dir+path.sep+entry[0])
-          await fsp.writeFile(file,JSON.stringify(entry[1])).catch(e=>console.error('Error when saving filename: ', e));
+          await fsp.writeFile(file,JSON.stringify(entry[1])).catch(e=>{console.error('Error when saving filename:'),console.error(e)});
           await file.close()
      }
  }
