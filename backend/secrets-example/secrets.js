47b5c45d4f0dd97b9574e2e7d2ec041ed10744da
Micah Powch <55720333+MeechaPooch@users.noreply.github.com>
2024-06-11 15:52:12 -0700

    Update secrets.js

--- backend/secrets-example/secrets.js
+++ backend/secrets-example/secrets.js
@@ -1,3 +1,5 @@
+// this is an example secrets file
+
 export const uptimeWebhookUrl = 'https://discord.com/api/webhooks/1250220100611604582/nMbrEiDNrCeth0uz8hp3KFrARtCfZ8Ml1-Xl9LrDW1iJ_7_hx4btEkmnYUV-ZqvBkR71'
 export const chatWebhookUrl = 'https://discord.com/api/webhooks/1250220100611604582/nMbrEiDNrCeth0uz8hp3KFrARtCfZ8Ml1-Xl9LrDW1iJ_7_hx4btEkmnYUV-ZqvBkR71'
 export const adminUser = {'username' : 'password'}
