40156b58dce9f67ce0e520278986748cd47a5b9c
Micah Powch <55720333+MeechaPooch@users.noreply.github.com>
2024-06-11 15:51:25 -0700

    Create example secrets.js

---
+++ backend/secrets-example/secrets.js
@@ -0,0 +1,9 @@
+export const uptimeWebhookUrl = 'https://discord.com/api/webhooks/1250220100611604582/nMbrEiDNrCeth0uz8hp3KFrARtCfZ8Ml1-Xl9LrDW1iJ_7_hx4btEkmnYUV-ZqvBkR71'
+export const chatWebhookUrl = 'https://discord.com/api/webhooks/1250220100611604582/nMbrEiDNrCeth0uz8hp3KFrARtCfZ8Ml1-Xl9LrDW1iJ_7_hx4btEkmnYUV-ZqvBkR71'
+export const adminUser = {'username' : 'password'}
+export const ids = {
+    projects:[
+        {id:'728098174',user:'Looky1173'},
+    ],
+}
+export const admin = ['griffpatch']
